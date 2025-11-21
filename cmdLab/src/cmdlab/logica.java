874b6dbb28cb58b1c444495fe3ff38754e7df17b
DJCC <david.carcamo2006@gmail.com>
2025-11-21 11:32:15 -0600

    aa

--- cmdLab/src/cmdlab/logica.java
+++ cmdLab/src/cmdlab/logica.java
@@ -33,91 +33,104 @@ public class logica extends comandos {
     
     
     public static String processCommand(String command){
-         
             String[] partes = command.split(" ",2);//probar que el espacio sirva como separador de comando
             String comando= partes[0];
             
-            switch(comando){
-                
-                case "Mkdir":
-                    if(partes.length<2 || partes[1].isEmpty()){
-                        return "Estructura Incorrecta";
-                    }
-                    
-                    String argumento = partes[1].trim();
-                    
-                    if(comandos.cmMkdir(argumento, ruta)){
-                        return "Directorio ["+argumento+"] creado Exitosamente";
-                    }else{
-                        return "NO SE PUDO CREAR DIRECTORIO";
-                    }
-                    
-                    
-                
-                    
-                case "Mfile":
-                    if(partes.length<2 || partes[1].isEmpty()){
-                       return ("Estructura incorrecta");
-                       
-                    }
-                    
-                    String argumentof = partes[1].trim();
-                    
-                    try{
-                        if(comandos.cmMfile(argumentof, ruta)){
-                            return "Archivo ["+argumentof+"] creado Exitosamente";
-                        }else{
-                            return "NO SE PUDO CREAR ARCHIVO";
-                        }
-                    }catch(IOException e){
-                        e.printStackTrace();
-                    }
-        
-                    
-                    
-                case "Rm":
-                    
-                    if(partes.length<2 || partes[1].isEmpty()){
-                        return ("Estructura incorrecta");
-                    }
-                    
-                    String argumentoR = partes[1].trim();
-                    
-                    if(comandos.cmRm(argumentoR, ruta)){
-                        return "Se elimino ["+argumentoR+"] Exitosamente";
-                    }else{
-                        return "NO SE PUDO ELIMINAR EL ARCHIVO";
-                    }
-                    
-                    
-                
-                case "Cd":
-                      if(partes.length<2 || partes[1].isEmpty()){
-                        return("Estructura incorrecta");
-                    }
-                    
-                    String argumentocd = partes[1].trim();
-                    return comandos.cmCd(argumentocd, ruta);
-                    
-                    
-                case "...":
-                    try{
-                       return comandos.cmReturn();
-                    }catch(IOException e2){
-                        return ("Error al volver");
-                    }
             
-                case "Dir":
-                    return comandos.cmDir();
-                    
-                case "Date":
-                    return comandos.cmDate();      
-                case "Time":
-                    return  comandos.cmTime();
-                    
-                default:
-                    return ("Comando Invalido "+comando);
+            
+            
+            
+            if(comando.equals("Mkdir")){
+             if(partes.length<2 || partes[1].isEmpty()){
+                    return "Estructura Incorrecta";
+                }
+
+                String argumento = partes[1].trim();
+
+                if(comandos.cmMkdir(argumento, ruta)){
+
+                    return "Directorio ["+argumento+"] creado Exitosamente";
+
+                }else{
+                    return "NO SE PUDO CREAR DIRECTORIO";
+                }
             }
+            
+            
+            
+            if(comando.equals("Mfile")){
+             if(partes.length<2 || partes[1].isEmpty()){
+                   return ("Estructura incorrecta");
+
+                }
+
+                String argumentof = partes[1].trim();
+
+                try{
+                    if(comandos.cmMfile(argumentof, ruta)){
+                        return "Archivo ["+argumentof+"] creado Exitosamente";
+                    }else{
+                        return "NO SE PUDO CREAR ARCHIVO";
+                    }
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+        
+                
+            }
+            if(comando.equals("Rm")){
+                  
+                if(partes.length<2 || partes[1].isEmpty()){
+                    return ("Estructura incorrecta");
+                }
+
+                String argumentoR = partes[1].trim();
+
+                if(comandos.cmRm(argumentoR, ruta)){
+                    return "Se elimino ["+argumentoR+"] Exitosamente";
+                }else{
+                    return "NO SE PUDO ELIMINAR EL ARCHIVO";
+                }
+                
+            }
+            if(comando.equals("Cd")){
+               if(partes.length<2 || partes[1].isEmpty()){
+                    return("Estructura incorrecta");
+                }
+
+                String argumentocd = partes[1].trim();
+                return comandos.cmCd(argumentocd, ruta);
+
+                    
+                
+            }
+            if(comando.equals("...")){
+              try{
+                   return comandos.cmReturn();
+                }catch(IOException e2){
+                    return ("Error al volver");
+                }
+            
+                
+            }
+            if(comando.equals("Dir")){
+                 return comandos.cmDir();
+                
+            }
+            if(comando.equals("Date")){
+                return comandos.cmDate();    
+                
+            }
+            if(comando.equals("Time")){
+                 return  comandos.cmTime();
+               
+                
+            }
+            
+           
+            return ("Comando Invalido "+comando);
+            
+         
     }
     
    public static File getRoot(){
